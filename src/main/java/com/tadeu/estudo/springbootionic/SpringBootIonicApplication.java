5ef4760cbfa2276608a588f64e80794fee2ed173
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-19 19:42:54 -0200

    Criando operacao de instanciacao com CommandLineRunner

--- src/main/java/com/tadeu/estudo/springbootionic/SpringBootIonicApplication.java
+++ src/main/java/com/tadeu/estudo/springbootionic/SpringBootIonicApplication.java
@@ -1,12 +1,31 @@
 package com.tadeu.estudo.springbootionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootIonicApplication {
+import com.tadeu.estudo.springbootionic.domain.Categoria;
+import com.tadeu.estudo.springbootionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringBootIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
