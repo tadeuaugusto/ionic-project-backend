633c856266854948889d637381b3412ecbea6b33
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-19 21:33:35 -0200

    Criacao ObjectNotFoundException e ajustes endpoint /categorias/{id}

---
+++ src/main/java/com/tadeu/estudo/springbootionic/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.tadeu.estudo.springbootionic.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.tadeu.estudo.springbootionic.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest req) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
