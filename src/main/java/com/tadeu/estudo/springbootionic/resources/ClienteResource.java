b34ea25f81fbc3653c3f280a328b62819dc4735e
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-20 00:56:14 -0200

    Referencia Ciclica Cliente x Endereco e Cidade x Estado e Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/tadeu/estudo/springbootionic/resources/ClienteResource.java
@@ -0,0 +1,42 @@
+package com.tadeu.estudo.springbootionic.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tadeu.estudo.springbootionic.domain.Cliente;
+import com.tadeu.estudo.springbootionic.domain.enums.TipoCliente;
+import com.tadeu.estudo.springbootionic.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Cliente> listar() {
+		Cliente c1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PF);
+		Cliente c2 = new Cliente(null, "Tadeu", "tadeu@gmail.com", "00000000000191", TipoCliente.PJ);
+		
+		List<Cliente> lista = new ArrayList<>();
+		lista.addAll(Arrays.asList(c1, c2));
+		
+		return lista;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/tadeu/estudo/springbootionic/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.tadeu.estudo.springbootionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tadeu.estudo.springbootionic.domain.Cliente;
+import com.tadeu.estudo.springbootionic.repositories.ClienteRepository;
+import com.tadeu.estudo.springbootionic.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
