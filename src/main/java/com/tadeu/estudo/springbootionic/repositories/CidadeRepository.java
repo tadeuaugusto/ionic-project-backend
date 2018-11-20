35974feac2dc885cda6b26ea86faa2112ca555e9
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-19 22:19:05 -0200

    Cidade e Estado

---
+++ src/main/java/com/tadeu/estudo/springbootionic/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.tadeu.estudo.springbootionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tadeu.estudo.springbootionic.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
