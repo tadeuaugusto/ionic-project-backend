5c83a72094673fcd4b099404526d33d299ab7b7b
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-22 17:50:05 -0200

    Validacao customizada na insercao de Cliente (FieldMessage: Email ja existente na base de dados)

--- src/main/java/com/tadeu/estudo/springbootionic/repositories/ClienteRepository.java
+++ src/main/java/com/tadeu/estudo/springbootionic/repositories/ClienteRepository.java
@@ -1,11 +1,15 @@
 package com.tadeu.estudo.springbootionic.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tadeu.estudo.springbootionic.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/tadeu/estudo/springbootionic/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/tadeu/estudo/springbootionic/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tadeu.estudo.springbootionic.domain.Cliente;
 import com.tadeu.estudo.springbootionic.domain.enums.TipoCliente;
 import com.tadeu.estudo.springbootionic.dto.ClienteNewDTO;
+import com.tadeu.estudo.springbootionic.repositories.ClienteRepository;
 import com.tadeu.estudo.springbootionic.resources.exception.FieldMessage;
 import com.tadeu.estudo.springbootionic.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public boolean isValid(ClienteNewDTO dto, ConstraintValidatorContext ctx) {
 		// TODO Auto-generated method stub
@@ -24,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (dto.getTipoCliente().equals(TipoCliente.PJ.getCod()) && !BR.isValidCNPJ(dto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CNPJ Invalido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(dto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente na base de dados"));
 		}
 		
 		for (FieldMessage e : list) {
