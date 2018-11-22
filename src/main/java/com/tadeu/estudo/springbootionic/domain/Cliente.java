8a28aa9acef29410cde2282d337bbfd8818079d0
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-22 16:46:22 -0200

    Ajuste CascadeType.ALL: permite delete de Cliente e Enderecos em cascata

--- src/main/java/com/tadeu/estudo/springbootionic/domain/Cliente.java
+++ src/main/java/com/tadeu/estudo/springbootionic/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -32,7 +33,7 @@ public class Cliente implements Serializable {
 	private Integer tipoCliente;
 	
 	// serialization allowed
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL) // CascadeType.ALL permite delete de Cliente e Enderecos em cascata
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
