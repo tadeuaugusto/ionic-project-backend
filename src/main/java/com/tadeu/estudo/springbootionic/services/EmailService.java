bb78d0b375d15f06d5b4ae77f1155c5405937e04
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-27 19:31:48 -0200

    Implementacao SmtpEmailService com servidor do Google e rodando em dev

--- src/main/java/com/tadeu/estudo/springbootionic/services/EmailService.java
+++ src/main/java/com/tadeu/estudo/springbootionic/services/EmailService.java
@@ -1,9 +1,11 @@
 package com.tadeu.estudo.springbootionic.services;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.tadeu.estudo.springbootionic.domain.Pedido;
 
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido pedido);
---
+++ src/main/java/com/tadeu/estudo/springbootionic/services/SmtpEmailService.java
@@ -0,0 +1,28 @@
+package com.tadeu.estudo.springbootionic.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmtpEmailService extends AbstractEmailService {
+
+	@Autowired
+	private MailSender mailSender;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		// TODO Auto-generated method stub
+		LOG.info("Teste de envio de email em DEV...");
+		
+		mailSender.send(msg);
+		LOG.info(msg.toString());
+		LOG.info("Email enviado!");
+	}
+
+}
