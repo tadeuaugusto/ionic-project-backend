42c57a34fde11044df6376b35ddea01fb693a901
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-11-27 23:26:57 -0200

    Salvando o primeiro arquivo no Amazoin S3

--- src/main/java/com/tadeu/estudo/springbootionic/SpringBootIonicApplication.java
+++ src/main/java/com/tadeu/estudo/springbootionic/SpringBootIonicApplication.java
@@ -1,11 +1,17 @@
 package com.tadeu.estudo.springbootionic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tadeu.estudo.springbootionic.services.S3Service;
+
 @SpringBootApplication
 public class SpringBootIonicApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicApplication.class, args);
@@ -14,5 +20,6 @@ public class SpringBootIonicApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		s3Service.uploadFile("C:\\tmp\\fotos\\PEACE-PIPE.jpg");
 	}
 }
---
+++ src/main/java/com/tadeu/estudo/springbootionic/services/S3Service.java
@@ -0,0 +1,42 @@
+package com.tadeu.estudo.springbootionic.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	@Autowired
+	private AmazonS3 s3client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String path) {
+		
+		try {
+			
+			File file = new File(path);
+			LOG.info("Iniciando upload");
+			s3client.putObject(new PutObjectRequest(bucketName, "peace-pipe.jpg", file));
+			LOG.info("Upload finalizado");
+		} catch (AmazonServiceException e) {
+			// TODO: handle exception
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("Status code: " + e.getErrorCode());
+		} catch (AmazonClientException e) {
+			LOG.info("AmazonClientException: " + e.getMessage());
+		}
+	}
+}
